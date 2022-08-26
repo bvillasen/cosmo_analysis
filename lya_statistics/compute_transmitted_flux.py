823fbbd798407dad5f311198a1246b44330cb74b
bruno villasenor <bvillasen@gmail.com>
2022-08-26 13:13:54 -0700

    Update compute_transmitted_flux.py

--- lya_statistics/compute_transmitted_flux.py
+++ lya_statistics/compute_transmitted_flux.py
@@ -1,7 +1,7 @@
 import os, sys
 import numpy as np
 import h5py as h5
-root_dir = os.getcwd() + '/'
+root_dir = os.path.dirname(os.getcwd()) + '/'
 subDirectories = [x[0] for x in os.walk(root_dir)]
 sys.path.extend(subDirectories)
 from tools import *
